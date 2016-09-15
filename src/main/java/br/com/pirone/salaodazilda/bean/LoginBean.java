c502ddd518f8a3beec6b422f344865f9392d612b
pirone <pedrohenriquedf@gmail.com>
2016-09-15 01:54:12 -0300

    Inicio da página login

---
+++ src/main/java/br/com/pirone/salaodazilda/bean/LoginBean.java
@@ -0,0 +1,46 @@
+package br.com.pirone.salaodazilda.bean;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class LoginBean {
+
+    private String username;
+    private String password;
+
+    /**
+     * @return the username
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * @param username the username to set
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    };
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /*public String login() {
+        if (username.equals(“madhav”) && password.equals("password")) {
+            return “success”;
+        } else {
+            return “failure”;
+    }*/
+
+}
